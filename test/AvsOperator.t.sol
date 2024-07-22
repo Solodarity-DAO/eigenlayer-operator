9b331f2d114448f8e148d9bacfb1d7fd1c508bd8
Michael DePetrillo <klassicd@gmail.com>
2024-07-22 13:45:01 -0700

    fix: test isValidSignature

---
+++ test/AvsOperator.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+import "../src/AvsOperator.sol";
+
+contract SolidarityAvsOperatorTest is Test {
+    AvsOperator avsOperator;
+    VmSafe.Wallet avsOperatorsManager;
+    VmSafe.Wallet ecdsaSigner;
+    bytes32 digestHash = keccak256("Hello, World!");
+
+    function setUp() public {
+        avsOperatorsManager = vm.createWallet("avsOperatorsManager");
+        ecdsaSigner = vm.createWallet("ecdsaSigner");
+
+        avsOperator = new AvsOperator();
+        avsOperator.initialize(avsOperatorsManager.addr);
+
+        vm.prank(avsOperatorsManager.addr);
+        avsOperator.updateEcdsaSigner(ecdsaSigner.addr);
+    }
+
+    function test_isValidSignature_Valid() public {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ecdsaSigner, digestHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        bytes4 magicValue = avsOperator.isValidSignature(digestHash, signature);
+        assertEq(magicValue, avsOperator.isValidSignature.selector);
+    }
+
+    function test_isValidSignature_Invalid() public {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ecdsaSigner, digestHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        signature[0] = 0;
+        bytes4 magicValue = avsOperator.isValidSignature(digestHash, signature);
+        assertEq(magicValue, bytes4(0xffffffff));
+    }
+}
