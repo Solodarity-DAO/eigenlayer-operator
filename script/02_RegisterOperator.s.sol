9706cab37cd69ae05c6d745c1d81bf719ff3ee4b
Michael DePetrillo <klassicd@gmail.com>
2024-07-25 09:34:31 -0700

    fix: operator registration

---
+++ script/02_RegisterOperator.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {IDelegationManager} from "@eigenlayer/interfaces/IDelegationManager.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+
+import {AvsOperatorsManager} from "../src/AvsOperatorsManager.sol";
+
+contract RegisterOperatorScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        address avsOperatorsManagerAddress = vm.envAddress("AVS_OPERATORS_MANAGER_ADDRESS");
+        AvsOperatorsManager operatorsManager = AvsOperatorsManager(avsOperatorsManagerAddress);
+
+        uint256 operatorId = vm.promptUint("Enter operator ID: ");
+        address earningsReceiver = vm.promptAddress("Enter earnings receiver address: ");
+        IDelegationManager.OperatorDetails memory operatorDetails = IDelegationManager.OperatorDetails({
+            __deprecated_earningsReceiver: earningsReceiver,
+            delegationApprover: address(0),
+            stakerOptOutWindowBlocks: 0
+        });
+        string memory metaDataURI =
+            "https://raw.githubusercontent.com/solidarity-dao/eigenlayer-operator/main/operator/metadata.json";
+
+        operatorsManager.registerAsOperator(operatorId, operatorDetails, metaDataURI);
+
+        vm.stopBroadcast();
+    }
+}
