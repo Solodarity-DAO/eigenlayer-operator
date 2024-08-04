12b56c0c3f8b6f7922eb23864e1fd68c5cb767c1
Michael DePetrillo <klassicd@gmail.com>
2024-08-04 09:46:07 -0700

    feat: update avs whitelist script

---
+++ script/UpdateAvsWhitelist.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+
+import {AvsOperatorsManager} from "../src/AvsOperatorsManager.sol";
+
+contract UpdateAvsWhitelistScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        address avsOperatorsManagerAddress = vm.envAddress("AVS_OPERATORS_MANAGER_ADDRESS");
+        AvsOperatorsManager avsOperatorsManager = AvsOperatorsManager(avsOperatorsManagerAddress);
+
+        address avsRegistryCoordinatorAddress = vm.envAddress("REGISTRY_COORDINATOR_ADDRESS");
+        uint256 operatorId = vm.promptUint("Enter operator ID: ");
+        avsOperatorsManager.updateAvsWhitelist(operatorId, avsRegistryCoordinatorAddress, true);
+
+        vm.stopBroadcast();
+    }
+}
